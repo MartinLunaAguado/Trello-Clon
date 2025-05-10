b3317a4aebfcc090535525dc5efb2dee5585ef97
Martin Luna Aguado <m.laguado@alumnos.upm.es>
2025-05-10 17:58:37 +0200

    overflow-x-auto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 
 <template>
    <main class="p-5 font-sans">
+    <div class="flex gap-5 py-5 overflow-x-auto">
       <div class="bg-gray-100 p-3 rounded-lg min-w-[250px] flex flex-col" >
         <h2 class="font-medium mb-2">Lista</h2>
-
+        <h2 class="font-medium mb-2">Lista</h2>
+        <h2 class="font-medium mb-2">Lista</h2>
+        <h2 class="font-medium mb-2">Lista</h2>
+        <h2 class="font-medium mb-2">Lista</h2>
+        <h2 class="font-medium mb-2">Lista</h2>
+        <h2 class="font-medium mb-2">Lista</h2>
+      </div>
+      
         </div>
   </main>
 </template>
